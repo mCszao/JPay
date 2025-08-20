5dfbcdb526ede51595205eddc35e55e041f96018
Marcos Vinicius <marc.khk11@gmail.com>
2025-08-20 19:21:52 -0400

    feat: add service de categorias

---
+++ src/main/java/com/challenge/JPay/service/CategoryService.java
@@ -0,0 +1,142 @@
+package com.challenge.JPay.service;
+
+import com.challenge.JPay.dto.request.CategoryRequestDTO;
+import com.challenge.JPay.dto.response.CategoryResponseDTO;
+import com.challenge.JPay.exception.BusinessException;
+import com.challenge.JPay.exception.CategoryNotFoundException;
+import com.challenge.JPay.exception.ResourceDuplicateException;
+import com.challenge.JPay.exception.ResourceNotFoundException;
+import com.challenge.JPay.model.Category;
+import com.challenge.JPay.repository.CategoryRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public Page<CategoryResponseDTO> findAll(Pageable pageable) {
+        log.info("Finding all categories with pagination: {}", pageable);
+
+        return repository.findAll(pageable)
+                .map(this::toResponseDTO);
+    }
+
+    public Page<CategoryResponseDTO> findAllActive(Pageable pageable) {
+        log.info("Finding all active categories");
+
+        return repository.findByActiveTrue(pageable)
+                .map(this::toResponseDTO);
+    }
+
+    public CategoryResponseDTO findById(Long id) {
+        log.info("Finding category by id: {}", id);
+
+        Category category = repository.findById(id)
+                .orElseThrow(() -> new CategoryNotFoundException(id.toString()));
+        return toResponseDTO(category);
+    }
+
+    public Page<CategoryResponseDTO> findByNameContaining(String name, Pageable pageable) {
+        log.info("Finding categories by name containing: {} with pagination: {}", name, pageable);
+
+        return repository.findByNameContainingIgnoreCase(name, pageable)
+                .map(this::toResponseDTO);
+    }
+
+    @Transactional
+    public CategoryResponseDTO create(CategoryRequestDTO dto) {
+        log.info("Creating new category: {}", dto.name());
+
+        if(repository.existsByNameIgnoreCase(dto.name())) {
+            throw new ResourceDuplicateException("Categoria "+ dto.name() + " já existe");
+        }
+
+        var createdCategory = repository.save(toEntity(dto));
+        log.info("Category created successfully with id: {}", createdCategory.getId());
+
+        return toResponseDTO(createdCategory);
+    }
+
+    @Transactional
+    public CategoryResponseDTO update(Long id, CategoryRequestDTO dto) {
+        log.info("Updating category with id: {}", id);
+
+        Category category = repository.findById(id)
+                .orElseThrow(() -> new CategoryNotFoundException(id.toString()));
+
+        if (!category.getName().equalsIgnoreCase(dto.name()) &&
+                repository.existsByNameIgnoreCase(dto.name())) {
+            throw new ResourceDuplicateException("Categoria "+ dto.name() + " já existe");
+        }
+
+        category.setName(dto.name());
+        category.setDescription(dto.description());
+
+        Category updatedCategory = repository.save(category);
+        log.info("Category updated successfully with id: {}", updatedCategory.getId());
+
+        return toResponseDTO(updatedCategory);
+    }
+
+    @Transactional
+    public void deactivate(Long id) {
+        log.info("Deactivating category with id: {}", id);
+
+        Category category = repository.findById(id)
+                .orElseThrow(() -> new CategoryNotFoundException(id.toString()));
+
+
+        long pendingAccountsCount = repository.countPendingAccountsByCategory(id);
+        if (pendingAccountsCount > 0) {
+            throw new BusinessException("Não é possivel desativar a categoria pois ela possuí" + pendingAccountsCount + " contas pendentes");
+        }
+
+        category.setActive(false);
+        repository.save(category);
+        log.info("Category deactivated successfully with id: {}", id);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        log.info("Deleting category with id: {}", id);
+
+        Category category = repository.findById(id)
+                .orElseThrow(() -> new CategoryNotFoundException(id.toString()));
+
+        long accountsCount = repository.countAccountsByCategory(id);
+        if (accountsCount > 0) {
+            throw new BusinessException("Não é possível deletar a categoria, pois ela possuí " + accountsCount + " contas a pagar associadas");
+        }
+
+        repository.delete(category);
+        log.info("Category deleted successfully with id: {}", id);
+    }
+
+    private Category toEntity(CategoryRequestDTO dto) {
+        return Category.builder()
+                .name(dto.name())
+                .description(dto.description())
+                .build();
+    }
+
+    private CategoryResponseDTO toResponseDTO(Category category) {
+        return CategoryResponseDTO.builder()
+                .id(category.getId())
+                .name(category.getName())
+                .description(category.getDescription())
+                .active(category.getActive())
+                .createdAt(category.getCreatedAt())
+                .updatedAt(category.getUpdatedAt())
+                .build();
+    }
+
+}
