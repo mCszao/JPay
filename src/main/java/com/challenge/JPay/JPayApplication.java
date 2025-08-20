e3f2f84ce66947e6b620e3bad850eb8a3f1eb828
Marcos Vinicius <marc.khk11@gmail.com>
2025-08-20 02:47:46 -0400

    feat: configuracao inicial

---
+++ src/main/java/com/challenge/JPay/JPayApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.JPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/JPay/JPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.JPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
